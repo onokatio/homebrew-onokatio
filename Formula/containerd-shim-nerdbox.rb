8745841051ab151d31ffdea7aff6039ad5603723
onokatio <contact@katio.net>
2025-12-13 20:27:12 +0900

    Add containerd for mac

---
+++ Formula/containerd-shim-nerdbox.rb
@@ -0,0 +1,32 @@
+class ContainerdShimNerdbox < Formula
+  desc "containerd sandbox runtime using vms"
+  homepage "https://github.com/containerd/nerdbox"
+  license "Apache-2.0"
+  head "https://github.com/containerd/nerdbox.git", branch: "main"
+
+  resource ("kernel") do
+    url "https://github.com/onokatio/nerdbox/releases/download/latest/nerdbox-kernel-arm64"
+    sha256 "e63f006c84e2f4a4a9676d10df8b0a5afa6dbfcb6bd8ab94403cc95dbc6f4dd7"
+  end
+  resource ("initrd") do
+    url "https://github.com/onokatio/nerdbox/releases/download/latest/nerdbox-initrd", using: :nounzip
+    sha256 "56fe4714b55e06c620a27a0fed6ae36fa206120d906147dbbb0f478eb1660f69"
+  end
+
+  depends_on "go" => :build
+  depends_on "e2fsprogs"
+
+  def install
+    system "make", "_output/containerd-shim-nerdbox-v1"
+    bin.install Dir["_output/*"]
+    resource("kernel").stage do
+      bin.install "./nerdbox-kernel-arm64"
+    end
+    resource("initrd").stage do
+      bin.install "./nerdbox-initrd"
+    end
+  end
+
+  test do
+  end
+end
---
+++ Formula/containerd.rb
@@ -0,0 +1,58 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://docs.brew.sh/rubydoc/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Containerd < Formula
+  desc "An open and reliable container runtime"
+  homepage "https://containerd.io"
+  url "https://github.com/containerd/containerd/archive/refs/tags/v2.2.0.tar.gz"
+  sha256 "86e7a268fc73f5332522baef86082c1d6c17986e2957a9ad842ead35d1080fca"
+  license "Apache-2.0"
+  head "https://github.com/containerd/containerd.git", branch: "main"
+
+  depends_on "go" => :build
+  depends_on "e2fsprogs"
+  depends_on "containerd-shim-nerdbox"
+
+  on_macos do
+    resource "config" do
+      url "https://raw.githubusercontent.com/containerd/nerdbox/6f751faad1a1c11088c4bfa714dd18e482b08d45/examples/macos/config.toml"
+      sha256 "b1ff3278f24cf5f72a9f579028c3ffa920e012a5f0083341f42f745971eb3c1c"
+    end
+  end
+
+  def install
+    ENV["PREFIX"] = prefix
+    system "make"
+    system "make install"
+  end
+
+  def post_install
+    if OS.mac?
+      # Install mac specific erofs config from nerdbox repo
+      resource("config").stage do
+        pkgetc.install "config.toml"
+      end
+    end
+  end
+
+  service do
+    run macos: [opt_bin/"containerd", "--config", etc/"containerd/config.toml"]
+    environment_variables PATH: "#{HOMEBREW_PREFIX}/opt/e2fsprogs/sbin:#{std_service_path_env}"
+  log_path var/"log/containerd/stdout.log"
+  error_log_path var/"log/containerd/stderr.log"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/ctr --version")
+    assert_match version.to_s, shell_output("#{bin}/containerd --version")
+  end
+
+  def caveats
+    <<~EOS
+      To use in MacOS, Please create yourself:
+
+      $ sudo mkdir /var/run/containerd
+      $ sudo chown $USER /var/run/containerd
+    EOS
+  end
+end
