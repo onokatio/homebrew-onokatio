034e60db760139c72d14013ebd1f3178f88654f4
onokatio <contact@katio.net>
2025-12-13 20:30:31 +0900

    nerdctl

---
+++ Formula/nerdctl.rb
@@ -0,0 +1,24 @@
+class Nerdctl < Formula
+  desc "ContaiNERD CTL - Docker-compatible CLI for containerd"
+  homepage "https://github.com/containerd/nerdctl"
+  url "https://github.com/containerd/nerdctl/archive/refs/tags/v2.2.0.tar.gz"
+  sha256 "105bead380f5f0d342506939d82793869604f61a88fdacd07b26e6a4d2521236"
+  license "Apache-2.0"
+  head "https://github.com/containerd/nerdctl.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X github.com/containerd/nerdctl/v2/pkg/version.Version=#{version}"
+    system "go", "build", *std_go_args(ldflags:), "./cmd/nerdctl"
+
+    generate_completions_from_executable(bin/"nerdctl", "completion")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/nerdctl --version")
+    output = shell_output("XDG_RUNTIME_DIR=/dev/null #{bin}/nerdctl images 2>&1", 1).strip
+    cleaned = output.gsub(/\e\[([;\d]+)?m/, "") # Remove colors from output
+    assert_match(/^time=.* level=fatal msg="rootless containerd not running.*/m, cleaned)
+  end
+end
