9d7c9076306aae1f0e763e6ea21f95e56afdef0e
tmaruyama <contact@katio.net>
2026-03-22 13:34:37 +0900

    Update nerdctl.rb

--- Formula/nerdctl.rb
+++ Formula/nerdctl.rb
@@ -1,7 +1,7 @@
 class Nerdctl < Formula
   desc "ContaiNERD CTL - Docker-compatible CLI for containerd"
   homepage "https://github.com/containerd/nerdctl"
-  url "https://github.com/containerd/nerdctl/archive/refs/tags/v2.2.0.tar.gz"
+  url "https://github.com/containerd/nerdctl/archive/refs/tags/v2.2.1.tar.gz"
   sha256 "105bead380f5f0d342506939d82793869604f61a88fdacd07b26e6a4d2521236"
   license "Apache-2.0"
   head "https://github.com/containerd/nerdctl.git", branch: "main"
